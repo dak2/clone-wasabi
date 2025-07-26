8509d27c0748fc2461c982de24a70326f4ee83a5
dak2 <dawt2h@gmail.com>
2025-07-26 14:05:21 +0000

    P72: hlt instructions

--- repo/wasabi/src/main.rs
+++ repo/wasabi/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 #![feature(offset_of)]
+
+use core::arch::asm;
 use core::mem::offset_of;
 use core::mem::size_of;
 use core::panic::PanicInfo;
@@ -81,18 +83,21 @@ struct EfiGraphicsOutputProtocol<'a> {
 
 fn locate_graphic_protocol<'a>(
     efi_system_table: &EfiSystemTable,
-    ) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
-        let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
-        let status = (efi_system_table.boot_services.locate_protocol)(
-            &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
-            null_mut::<EfiVoid>(),
-            &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol
-            as *mut *mut EfiVoid,
-        );
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+    let status = (efi_system_table.boot_services.locate_protocol)(
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol as *mut *mut EfiVoid,
+    );
     if status != EfiStatus::Success {
         return Err("Failed to locate graphics output protocol");
     }
     Ok(unsafe { &*graphic_output_protocol })
+}
+
+pub fn hlt() {
+    unsafe { asm!("hlt") }
 }
 
 #[no_mangle]
@@ -101,19 +106,20 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
     let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
     let vram = unsafe {
-        slice::from_raw_parts_mut(
-            vram_addr as *mut u32,
-            vram_byte_size / size_of::<u32>(),
-        )
+        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
     };
     for e in vram {
-    *e = 0xffffff;
-}
+        *e = 0xffffff;
+    }
     //println!("Hello, world!");
-    loop {}
+    loop {
+        hlt()
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    loop {
+        hlt()
+    }
 }
